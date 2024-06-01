9a1b30a5282fa3740b6be809de6dc2a10029dbfa
kyujenius <hkj020607@gmail.com>
2024-06-01 21:09:24 +0900

    fix: comment 적용 (상단 바)

--- lib/view/chatting/chatting_list_screen.dart
+++ lib/view/chatting/chatting_list_screen.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:pill_on_phone/config/font_system.dart'; // Make sure to import this for font styles
 import 'package:pill_on_phone/view/base/base_screen.dart';
 import 'package:pill_on_phone/view/chatting/widget/chatting_list_widget.dart';
 import 'package:pill_on_phone/view_model/chatting/chatting_view_model.dart';
+import 'package:pill_on_phone/widget/app_bar/default_app_bar.dart';
 
 class ChattingListScreen extends BaseScreen<ChattingViewModel> {
   const ChattingListScreen({super.key});
+
+  @override
+  PreferredSizeWidget? buildAppBar(BuildContext context) {
+    return const PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: DefaultAppBar(
+          title: '채팅 리스트',
+        ));
+  }
 
   @override
   Widget buildBody(BuildContext context) {
@@ -15,13 +24,6 @@ class ChattingListScreen extends BaseScreen<ChattingViewModel> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Padding(
-          padding: EdgeInsets.only(top: 32.0, left: 16.0, right: 16.0),
-          child: Text(
-            "채팅",
-            style: FontSystem.H1,
-          ),
-        ),
         Expanded(
           child: Obx(() {
             if (viewModel.isChatListLoading.value) {
